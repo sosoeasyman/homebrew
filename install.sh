7394a99461aef01868de08fd708a60357c108053
dick.wang <sosoeasyman@gmail.com>
2015-10-12 12:00:56 +0800

    My install script.

---
+++ install.sh
@@ -0,0 +1,40 @@
+brew cask install java
+brew cask install eclipse-jee
+brew cask install android-studio
+brew cask install jd-gui
+
+brew cask install textmate
+brew cask install macdown
+brew cask install xmind
+
+brew cask install robomongo
+brew cask install pgadmin3
+brew cask install sqlitebrowser
+
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install genymotion
+
+brew cask install cord
+brew cask install teamviewer
+brew cask install xquartz
+brew cask install filezilla
+brew cask install handbrake
+
+brew cask install firefox
+brew cask install google-chrome	
+brew cask install flash
+
+brew cask install skype
+brew cask install spotify
+brew cask install keka
+
+brew install ant
+brew install maven
+brew install mongodb
+brew install nmap
+brew install ssh-copy-id
+brew install tree
+brew install git
+brew install git-flow
+brew install subversion
